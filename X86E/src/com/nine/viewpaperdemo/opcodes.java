b8ebbaf2f6d434fa0bf3bb49f1b4512d97c3dcde
mongolia19 <mongolia19@gmail.com>
2014-12-01 20:17:30 +0800

    Add cpu8086.java to operat cpu regs

--- X86E/src/com/nine/viewpaperdemo/opcodes.java
+++ X86E/src/com/nine/viewpaperdemo/opcodes.java
@@ -65,7 +65,7 @@ public static int  op_execute( CPU_STATE_S cpu )
 		{
 			skip += ( bit_counter >> ( 7 - bit_index ) ) & 1;
 		}
-		//Toast.makeText(, text, duration)	("skip for opcode %x is %d [ addr %04x -> %04x ]\n", opcode, skip, preIP, cpu->IP + skip);
+		//Toast.makeText(, text, duration)	("skip for opcode %x is %d [ addr %04x . %04x ]\n", opcode, skip, preIP, cpu.IP + skip);
 		
 		return skip;
 
@@ -111,7 +111,7 @@ private static int op0a(CPU_STATE_S cpu) {
 	dst = src1 | src2;
 	set_ea_val_8(cpu, dst);
 	flags.set_flag_log_byte(cpu, dst);
-	//printf( "OR %s, %s\n", byteRegName[ cpu->modrm.bit.rm ], byteRegName[ cpu->modrm.bit.reg ] );
+	//printf( "OR %s, %s\n", byteRegName[ cpu.modrm.bit.rm ], byteRegName[ cpu.modrm(CPU_STATE_S.GetBit, CPU_STATE_S.reg) ] );
 	return cpu.cnt;
 }
 
@@ -141,7 +141,7 @@ static int op20( CPU_STATE_S cpu )
 	dst = src1 & src2;
 	set_ea_val_8(cpu, dst);
 	flags.set_flag_log_byte( cpu, dst );
-	//printf("AND %s, %s\n", byteRegName[ cpu->modrm.bit.reg ], "???" );
+	//printf("AND %s, %s\n", byteRegName[ cpu.modrm(CPU_STATE_S.GetBit, CPU_STATE_S.reg) ], "???" );
 	return cpu.cnt;
 }
 
@@ -155,7 +155,7 @@ static int op21( CPU_STATE_S cpu )
 	dst = src1 & src2;
 	set_ea_val_16(cpu, dst);
 	flags.set_flag_log_word(cpu, dst );
-	//printf("AND %s, %s\n", wordRegName[ cpu->modrm.bit.reg ], "???" );
+	//printf("AND %s, %s\n", wordRegName[ cpu.modrm(CPU_STATE_S.GetBit, CPU_STATE_S.reg) ], "???" );
 	return cpu.cnt;
 }
 
@@ -169,7 +169,7 @@ static int op29( CPU_STATE_S cpu )
 	dst = src1 - src2;
 	set_ea_val_16( cpu, dst );
 	flags.set_flag_sub_word( cpu, src1, src2 );
-	//printf( "SUB %s %s\n", wordRegName[ cpu->modrm.bit.reg ], "???" );
+	//printf( "SUB %s %s\n", wordRegName[ cpu.modrm(CPU_STATE_S.GetBit, CPU_STATE_S.reg) ], "???" );
 	return cpu.cnt;
 }
 
@@ -186,9 +186,654 @@ static int op2c( CPU_STATE_S cpu )
 }
 //TODO 2c finished
 ///
+//XOR rm16, reg16
+static int op31( CPU_STATE_S cpu )
+{
+	int src1, src2, dst;
+	cpu.cnt |= BCNT_1; // modrm
+	src1 = get_ea_val_16( cpu );
+	src2 = get_reg_val_16( cpu, cpu.modrm(CPU_STATE_S.GetBit, CPU_STATE_S.reg) );
+	dst = src1 ^ src2;
+	set_ea_val_16(cpu, dst);
+	flags.set_flag_log_word(cpu, dst);
+	//printf( "XOR %s, %s\n", wordRegName[ cpu.modrm.bit.rm ], wordRegName[ cpu.modrm(CPU_STATE_S.GetBit, CPU_STATE_S.reg) ] );
+	return cpu.cnt;
+}
 
+//CMP 	rm16, reg16
+static int op39( CPU_STATE_S cpu )
+{	
+	int src1, src2;
+	cpu.cnt |= BCNT_1; // modrm
+	src1 = get_ea_val_16( cpu );
+	src2 = get_reg_val_16( cpu, cpu.modrm(CPU_STATE_S.GetBit, CPU_STATE_S.reg) );
+	flags.set_flag_sub_word( cpu, src1, src2);
+	//printf( "CMP %s, %s\n", wordRegName[ cpu.modrm.bit.rm ], wordRegName[ cpu.modrm(CPU_STATE_S.GetBit, CPU_STATE_S.reg) ] );
+	return cpu.cnt;
+}
+
+//CMP 	AL 	Ib
+static int op3c( CPU_STATE_S cpu )
+{
+	int src1, src2;
+	src1 = get_reg_val_8(cpu, REG_AL);
+	src2 = cpu.mp.mp[1];
+	cpu.cnt |= BCNT_1; // src2
+	flags.set_flag_sub_byte(cpu, src1, src2);
+	return cpu.cnt;
+}
+
+//INC 	reg16
+static int op40( CPU_STATE_S cpu )
+{
+	int rm = MODRM_RM(cpu.mp.mp[0]);
+	int src = get_reg_val_16(cpu, rm);
+	set_reg_val_16( cpu, rm, src + 1 );
+	// we can't modify the CF flag
+	int cf =cpu.get_flag_bit(CPU_STATE_S.CF) ;
+	flags.set_flag_add_word( cpu, src, 1 );
+	cpu.flags(( cf != 0 ), CPU_STATE_S.CF);
+	//cpu.flags.bit.CF = ( cf != 0 );
+	//printf( "INC %s\n", wordRegName[rm]);
+	return cpu.cnt;
+}
+
+
+
+//DEC 	reg16
+static int op48( CPU_STATE_S cpu )
+{
+	int rm = MODRM_RM(cpu.mp.mp[0]);
+	int src = get_reg_val_16(cpu, rm);
+	set_reg_val_16( cpu, rm, src - 1 );
+	// we can't modify the CF flag
+	int cf = cpu.get_flag_bit(CPU_STATE_S.CF) ;
+	flags.set_flag_sub_word( cpu, src, 1 );
+	cpu.flags(( cf != 0 ), CPU_STATE_S.CF);
+	//printf( "DEC %s\n", wordRegName[rm]);
+	return cpu.cnt;
+}
+
+//PUSH
+static int op50( CPU_STATE_S cpu )
+{
+	int rm = MODRM_RM(cpu.mp.mp[0]);
+	//printf( "PUSH %s\n", wordRegName[rm] );	
+	if( rm == REG_SP /* && CPU_PUSH_FIRST?? */ )
+	{
+		cpu8086.cpu_push( cpu, cpu.SP - 2 );
+	}
+	else
+	{
+		int value = get_reg_val_16( cpu, rm );
+		cpu8086. cpu_push( cpu, value );
+	}
+	return cpu.cnt;
+}
+
+//POP 
+static int op58( CPU_STATE_S cpu )
+{
+	int rm = MODRM_RM(cpu.mp.mp[0]);
+	int val = cpu8086.cpu_pop( cpu );
+	set_reg_val_16( cpu, rm, val );
+	//printf( "POP %s\n", wordRegName[rm] );	
+	return cpu.cnt;
+}
+
+//JB/JNAE/JC imm8
+static int op72( CPU_STATE_S cpu )
+{
+	int mem = ADD_S8( cpu.IP + 2, cpu.mp.mp[1] );
+	cpu.cnt |= BCNT_1;
+	//printf("JC %xh\n", mem);
+	if( (cpu.get_flag_bit(CPU_STATE_S.CF)>0) )
+	{
+		cpu.IP = mem;
+		cpu.mp.mp[mem] = cpu.memory.mem[ mem ];
+		return 0; // jump
+	}
+	return cpu.cnt; // no jump
+}
+
+
+
+//JZ/JE imm8
+static int op74( CPU_STATE_S cpu )
+{
+	int mem = ADD_S8( cpu.IP + 2, cpu.mp.mp[1] );
+	cpu.cnt |= BCNT_1; // modrm
+	//printf("JZ %xh\n", mem);
+	
+	if(cpu.get_flag_bit(CPU_STATE_S.ZF)>0  )
+	{
+		cpu.IP = mem;
+		cpu.mp.mp [mem]= cpu.memory.mem[ mem ];
+		return 0; // jump
+	}
+	return cpu.cnt; // no jump
+}
+
+//JNZ/JNE imm8
+static int op75( CPU_STATE_S cpu )
+{
+	int mem = ADD_S8( cpu.IP + 2, cpu.mp.mp[1] );
+	cpu.cnt |= BCNT_1;
+	//printf("JNZ %xh\n", mem);
+	if( (cpu.get_flag_bit(CPU_STATE_S.ZF)==0) )
+	{
+		cpu.IP = mem;
+		cpu.mp.mp[mem] = cpu.memory.mem[ mem ];
+		return 0; // jump
+	}
+	return cpu.cnt; // no jump	
+}
+
+//JBE/JNA imm8
+static int op76( CPU_STATE_S cpu )
+{
+	int mem = ADD_S8( cpu.IP + 2, cpu.mp.mp[1] );
+	cpu.cnt |= BCNT_1;
+	//printf("JBE 0x%02x\n", mem);
+	
+	if(cpu.get_flag_bit(CPU_STATE_S.CF)>0 ||cpu.get_flag_bit(CPU_STATE_S.ZF)>0 )// cpu.flags.bit.CF || cpu.flags.bit.ZF )
+	{
+		cpu.IP = mem;
+		cpu.mp.mp[mem] = cpu.memory.mem[ mem ];
+		return 0; // jump
+	}
+	return cpu.cnt; // no jump
+}
+
+//JA imm8
+static int op77( CPU_STATE_S cpu )
+{
+	int mem = ADD_S8( cpu.IP + 2, cpu.mp.mp[1] );
+	cpu.cnt |= BCNT_1;
+	//printf("JA 0x%02x\n", mem);
+	if( !( cpu.flags.bit.CF || cpu.flags.bit.ZF ) )
+	{
+		cpu.IP = mem;
+		cpu.mp = &cpu.memory[ mem ];
+		return 0; // jump
+	}
+	return cpu.cnt; // no jump
+}
+
+//JNS imm8
+static int op79( CPU_STATE_S cpu )
+{
+	int mem = ADD_S8( cpu.IP + 2, cpu.mp.mp[1] );
+	cpu.cnt |= BCNT_1;
+	//printf("JNS 0x%02x\n", mem);
+	if( !cpu.flags.bit.SF )
+	{
+		cpu.IP = mem;
+		cpu.mp.mp[mem] = cpu.memory.mem[ mem ];///Here maybe a problem
+		return 0; // jump
+	}
+	return cpu.cnt; // no jump
+}
+
+//Various rm8, imm8
+static int op80( CPU_STATE_S cpu )
+{
+	int src1, src2, dst, offset;
+	cpu.cnt |= BCNT_1;
+	switch( cpu.modrm(CPU_STATE_S.GetBit, CPU_STATE_S.reg) )
+	{
+		case 0x1: // OR rm8, imm8
+			src1 = get_ea_val_8(cpu);
+			offset = bit_count( cpu.cnt );
+			src2 = cpu.mp.mp[offset];
+			cpu.cnt |= (1<<(offset));
+			dst = src1 | src2;
+			set_ea_val_8( cpu, dst);
+			flags.set_flag_log_byte( cpu, dst );
+			//printf("ORB(80) %xh %xh\n", src1, src2);
+		break;
+		
+		default:
+			//printf("reg = %x\n", cpu.modrm(CPU_STATE_S.GetBit, CPU_STATE_S.reg) );
+			return UNHANDLED_OPCODE;
+		break;
+	}
+	return cpu.cnt;
+}
+
+//Various rm16, imm16
+static int op81( CPU_STATE_S cpu )
+{
+	int src1, src2, offset;
+	cpu.cnt |= BCNT_1;
+	src1 = get_ea_val_16( cpu );
+	offset = bit_count( cpu.cnt );
+	src2 = MAKE16( cpu.mp.mp[offset+1], cpu.mp.mp[offset] );;
+	cpu.cnt |= (1<<(offset)) | (1<<(offset+1));
+	switch( cpu.modrm(CPU_STATE_S.GetBit, CPU_STATE_S.reg) )
+	{
+		case 0x7: // CMP rm16, imm16
+			//printf("CMP(81) (%xh) %xh\n", src1, src2);
+			flags.set_flag_sub_word( cpu, src1, src2 );
+		break;
+		
+		default:
+			//printf("reg = %x\n", cpu.modrm(CPU_STATE_S.GetBit, CPU_STATE_S.reg) );
+			return UNHANDLED_OPCODE;
+		break;
+	}
+	return cpu.cnt;
+}
+
+private static int bit_count(int Byte) {
+	
+	
+		int bit_index, count = 0;
+		for( bit_index = 0; bit_index < 8; ++bit_index )
+			count += ( Byte >> ( 7 - bit_index ) ) & 1;
+		return count;
+
+}
+
+//Various rm16, imm8
+static int op83( CPU_STATE_S cpu )
+{
+	int src1, src2, dst, offset;
+	cpu.cnt |= BCNT_1; // modrm
+	src1 = get_ea_val_16(cpu);
+	offset = bit_count( cpu.cnt );
+	src2 = MAKE_S16( cpu.mp.mp[offset] );
+	cpu.cnt |= (1<<(offset));
+	
+	switch( cpu.modrm(CPU_STATE_S.GetBit, CPU_STATE_S.reg) ) 
+	{
+		case 0x0: // ADD rm16, imm8
+			//printf("ADD %s(0x%04x) %xh\n", wordRegName[cpu.modrm.bit.rm], src1, src2);
+			dst = src1 + src2;
+			set_ea_val_16(cpu, dst);
+			flags.set_flag_add_word(cpu, src1, src2);
+			break;
+			
+		case 0x1: // OR rm16, imm8
+			return UNHANDLED_OPCODE;
+			break;
+			
+		case 0x2: // ADC rm16, imm8
+			//printf("ADC %s(0x%04x) %xh\n", wordRegName[cpu.modrm.bit.rm], src1, src2);
+			dst = src1 +  src2 + cpu.flags.bit.CF;
+			set_ea_val_16( cpu, dst );
+			set_flag_adc_word( cpu, src1, src2, cpu.flags.bit.CF );
+			break;
+			
+		case 0x3: // SBB rm16, imm8
+			return UNHANDLED_OPCODE;
+			break;
+			
+		case 0x4: // AND rm16, imm8 
+			//printf("AND %s(0x%04x) %xh\n", wordRegName[cpu.modrm.bit.rm], src1, src2);
+			dst = src1 & src2;
+			set_ea_val_16( cpu, dst );
+			flags.set_flag_log_word(cpu, dst);
+			break;
+			
+		case 0x5: // SUB rm16, imm8
+			//printf("SUB %s(0x%04x) %xh\n", wordRegName[cpu.modrm.bit.rm], src1, src2);
+			dst = src1 - src2;
+			set_ea_val_16( cpu, dst );
+			flags.set_flag_sub_word(cpu, src1, src2);
+			break;
+			
+		case 0x6: // XOR /m16, imm8
+			//printf("XOR %s(0x%04x) %xh\n", wordRegName[cpu.modrm.bit.rm], src1, src2);
+			dst = src1 ^ src2;
+			set_ea_val_16( cpu, dst );
+			flags.set_flag_log_word(cpu, dst);
+			break;
+			
+		case 0x7: // CMP rm16, imm8
+			//printf("CMP %s(0x%04x) %xh\n", wordRegName[cpu.modrm.bit.rm], src1, src2);
+			flags.set_flag_sub_word( cpu, src1, src2 );
+			break;
+	}
+	return cpu.cnt;
+}
+
+
+//XCHG  rm8, reg8
+static int op86( CPU_STATE_S cpu )
+{
+	int src1 = get_ea_val_8(cpu);
+	int src2 = get_reg_val_8(cpu, cpu.modrm(CPU_STATE_S.GetBit, CPU_STATE_S.reg));
+	cpu.cnt |= BCNT_1; // modrm
+	set_ea_val_8( cpu, src2);
+	set_reg_val_8(cpu, cpu.modrm(CPU_STATE_S.GetBit, CPU_STATE_S.reg), src1);
+	//printf( "XCHG %s, %s\n", byteRegName[ cpu.modrm(CPU_STATE_S.GetBit, CPU_STATE_S.reg) ], "???" );
+	return cpu.cnt;
+}
+
+//XCHG  rm16, reg16
+static int op87( CPU_STATE_S cpu )
+{
+	int src1 = get_ea_val_16(cpu);
+	int src2 = get_reg_val_16(cpu, cpu.modrm(CPU_STATE_S.GetBit, CPU_STATE_S.reg));
+	cpu.cnt |= BCNT_1; // modrm
+	set_ea_val_16(cpu, src2);
+	set_reg_val_16(cpu, cpu.modrm(CPU_STATE_S.GetBit, CPU_STATE_S.reg), src1);
+	//printf( "XCHG %s, %s\n", wordRegName[ cpu.modrm(CPU_STATE_S.GetBit, CPU_STATE_S.reg) ], "???" );
+	return cpu.cnt;
+}
+
+//MOV rm8, reg8
+static int op88( CPU_STATE_S cpu )
+{
+	int val = get_reg_val_8( cpu, cpu.modrm(CPU_STATE_S.GetBit, CPU_STATE_S.reg) );
+	cpu.cnt |= BCNT_1; // modrm
+	//printf("MOV [BX+DI](%xh) %s(0x%x)\n", cpu.BX + cpu.DI, byteRegName[ cpu.modrm(CPU_STATE_S.GetBit, CPU_STATE_S.reg) ], val);
+	set_ea_val_8(cpu, val);
+	return cpu.cnt;
+}
+
+//MOV rm16, reg16
+static int op89( CPU_STATE_S cpu )
+{
+	int val = get_reg_val_16( cpu, cpu.modrm(CPU_STATE_S.GetBit, CPU_STATE_S.reg) );
+	int addr = MAKE16( cpu.mp.mp[3], cpu.mp.mp[2] );
+	cpu.cnt |= BCNT_1;
+	//printf("MOV [%xh] %s(0x%x)\n", addr, wordRegName[ cpu.modrm(CPU_STATE_S.GetBit, CPU_STATE_S.reg) ], val);
+	set_ea_val_16( cpu, val );
+	return cpu.cnt;
+}
+
+//MOV reg8, rm8
+static int op8a( CPU_STATE_S cpu )
+{
+	int val = get_ea_val_8(cpu);
+	cpu.cnt |= BCNT_1; // modrm
+	set_reg_val_8(cpu, cpu.modrm(CPU_STATE_S.GetBit, CPU_STATE_S.reg), val);
+	//printf("MOV %s 0x%02x \n", byteRegName[ cpu.modrm(CPU_STATE_S.GetBit, CPU_STATE_S.reg) ], val );
+	return cpu.cnt;
+}
+
+//MOV reg16, rm16
+static int op8b( CPU_STATE_S cpu )
+{
+	int val = get_ea_val_16(cpu);
+	cpu.cnt |= BCNT_1;
+	set_reg_val_16(cpu, cpu.modrm(CPU_STATE_S.GetBit, CPU_STATE_S.reg), val );
+	//printf("MOV %s 0x%04x \n", wordRegName[ cpu.modrm(CPU_STATE_S.GetBit, CPU_STATE_S.reg) ], val );
+	return cpu.cnt;
+}
+
+//XCHG AX, reg16
+static int op90( CPU_STATE_S cpu )
+{
+	int rm = MODRM_RM(cpu.mp.mp[0]);
+	int val = cpu.AX;
+	val = cpu.AX;
+	cpu.AX = get_reg_val_16( cpu, rm );
+	set_reg_val_16( cpu, rm, val );
+	//printf( "NOP/XCHG AX, %s\n", wordRegName[ rm ] );
+	return cpu.cnt;
+}
+
+
+//MOV reg8, imm8
+static int opb0( CPU_STATE_S cpu )
+{
+	int rm = MODRM_RM( cpu.mp.mp[0] );
+	int val = cpu.mp.mp[1];
+	cpu.cnt |= BCNT_1;
+	set_reg_val_8(cpu, rm, val);
+	printf("MOV %s, $%x\n", byteRegName[rm], val);
+	return cpu.cnt;
+}
+
+
+//MOV reg16, imm16
+static int opb8( CPU_STATE_S cpu )
+{
+	int rm = MODRM_RM( cpu.mp.mp[0] );
+	int val =  MAKE16( cpu.mp.mp[2], cpu.mp.mp[1] );
+	cpu.cnt |= BCNT_1 | BCNT_2;
+	set_reg_val_16(cpu, rm, val);
+	//printf("MOV %s, $%x\n", wordRegName[rm], val);
+	return cpu.cnt;
+}
+
+//RETN
+static int opc3( CPU_STATE_S cpu )
+{
+	int addr =cpu8086.cpu_pop( cpu );
+	//printf( "RETN\n");
+	cpu.IP = addr;
+	cpu.mp = &cpu.memory[ addr ];
+	return 0;
+}
+
+//LES reg16, rm16
+static int opc4( CPU_STATE_S cpu )
+{
+	int val1, val2;
+	cpu.cnt |= BCNT_1 | BCNT_2 | BCNT_3 | BCNT_4 | BCNT_5;
+	val1 = MAKE16( cpu.mp.mp[3], cpu.mp.mp[2] );
+	val2 = MAKE16( cpu.mp.mp[5], cpu.mp.mp[4] );
+	set_reg_val_16(cpu, cpu.modrm(CPU_STATE_S.GetBit, CPU_STATE_S.reg), val1);
+	cpu.ES = val2;
+	return cpu.cnt;
+}
+
+//MOV(W) rm16, data16
+static int opc7( CPU_STATE_S cpu )
+{
+	// @TODO: set the offsets based on modrm displacements instead of hard coding the 5,4 indexes here
+	int val = MAKE16( cpu.mp.mp[5], cpu.mp.mp[4] );
+	cpu.cnt |= BCNT_1 | BCNT_4 | BCNT_5 ;
+	set_ea_val_16(cpu, val);
+	return cpu.cnt;
+}
+
+//various Ev CL
+static int opd3( CPU_STATE_S cpu )
+{
+	int src, dst;
+	int cl = get_reg_val_8(cpu, REG_CL );
+	cpu.cnt |= BCNT_1; // modrm
+	src = get_ea_val_16(cpu);
+	//cl &= 0x1f;
+	if( cl == 0x0 )
+		return cpu.cnt;
+	switch( cpu.modrm(CPU_STATE_S.GetBit, CPU_STATE_S.reg) )
+	{
+		case 0x0: // ROL
+			dst = ( src << ( cl & 15 ) ) | ( src >> ( 16 - ( cl & 15 ) ) );
+			cpu.flags.bit.CF  = ( ( dst & 1 ) != 0 );
+			break;
+		case 0x1: // ROR
+			return UNHANDLED_OPCODE;
+			break;
+		case 0x2: // RCL
+			return UNHANDLED_OPCODE;
+			break;
+		case 0x3: // RCR
+			return UNHANDLED_OPCODE;
+			break;
+		case 0x4: // SHL
+		case 0x6: // alias
+			return UNHANDLED_OPCODE;
+			break;
+		case 0x5: // SHR
+			return UNHANDLED_OPCODE;
+			break;
+		case 0x7: // SAR
+			return UNHANDLED_OPCODE;
+			break;
+	}
+	cpu.flags.bit.OF = ( ( ( dst ^ src) & 0x8000 ) != 0 );
+	set_ea_val_16(cpu, (int)dst);
+	return cpu.cnt;
+}
+
+//LOOPNZ//LOOPNE imm8
+static int ope0( CPU_STATE_S cpu )
+{
+	cpu.cnt |= BCNT_1;
+	cpu.CX -= 1;
+	if( cpu.CX != 0 && !cpu.flags.bit.ZF )
+	{
+		cpu.IP = ADD_S8(cpu.IP, cpu.mp.mp[1] + 2);
+		cpu.mp.mp = cpu.memory.mem[ cpu.IP ];
+		return 0;
+	}
+	return cpu.cnt;
+}
+
+//CALL imm16
+static int ope8( CPU_STATE_S cpu )
+{
+	int mem =  MAKE16( cpu.mp.mp[2], cpu.mp.mp[1] );
+	cpu.cnt |= BCNT_1 | BCNT_2;
+	cpu8086.cpu_push( cpu, cpu.IP + 3 );
+	cpu.IP += 3 + mem;
+	cpu.mp = &cpu.memory[ cpu.IP ];
+	//printf( "CALL %xh\n", cpu.IP  );
+	// push the current address(well the next location to execute when the stack is popped) to the stack
+	// then set the IP to the new location in memory at imm16
+	return 0;
+}
+
+//JMP imm8
+static int opeb( CPU_STATE_S cpu )
+{
+	cpu.IP = ADD_S8( cpu.IP, cpu.mp.mp[1] + 2 );
+	cpu.mp = &cpu.memory[ cpu.IP ];
+	cpu.cnt |= BCNT_1;
+	//printf( "JMP %xh\n", cpu.IP );
+	return 0;
+}
+
+//HALT
+static int opf4( CPU_STATE_S cpu )
+{
+	//printf( "HALT!\n");
+	return UNHANDLED_OPCODE; 
+}
+
+static int opf7( CPU_STATE_S cpu )
+{
+	int src1, src2;
+	cpu.cnt |= BCNT_1; // modrm
+	//@TODO: once support for segmented memory is in, this will need to be reintegrated
+	//src1 = ( (uint32_t)cpu.DX << 16 ) | cpu.AX;
+	src1 = cpu.AX;
+	src2 = get_ea_val_16( cpu );
+	switch( cpu.modrm(CPU_STATE_S.GetBit, CPU_STATE_S.reg) )
+	{
+		case 0x0:
+			break;
+		case 0x1:
+			break;
+		case 0x2:
+			break;
+		case 0x3:
+			break;
+		case 0x4:
+			break;
+		case 0x5:
+			break;
+		case 0x6:
+			break;
+		case 0x7: // IDIV rm16
+		{
+			int sign1, sign2, dst1, dst2;
+			//printf("IDIV rm16\n");
+			// TODO: same thing as above (segmentation)
+			//src2 = ( src2 & 0x8000 ) ? ( src2 | 0xffff0000 ) :  src2;
+			if( src2 == 0 )
+			{
+				//printf("DIVIDE ERROR\n");
+			}
+			sign1 = ( ( src1 & 0x80000000 ) != 0 );
+			sign2 = ( ( src2 & 0x80000000 ) != 0 );
+			dst1 = (int16_t)src1 / (int16_t)src2;
+			dst2 = (int16_t)src1 % (int16_t)src2;
+			if( sign1 != sign2 )
+			{
+				if( dst1 > 0x8000 )
+				{
+					//printf("DIVIDE ERROR\n");
+				}
+				dst1 = (( ~dst1 + 1 ) & 0xffff);
+			}
+			else
+			{
+				if( dst1 > 0x7fff )
+				{
+					//printf("DIVIDE ERROR\n");					
+				}
+				if( sign1 )
+				{
+					dst2 = (int16_t)(( ~dst2 + 1 ) & 0xffff);
+				}
+			}
+			cpu.AX = (int)dst1;
+			cpu.DX = (int)dst2;
+			return cpu.cnt;
+		}	
+			break;
+	}
+	return UNHANDLED_OPCODE;
+}
+
+
+//CLC
+static int opf8( CPU_STATE_S cpu )
+{
+	cpu.flags(CPU_STATE_S.CLEAR_BIT,CPU_STATE_S.CF);
+	// cpu.flags.bit.CF = 0;
+	return cpu.cnt;
+}
+
+//STC
+static int opf9( CPU_STATE_S cpu )
+{
+	cpu.flags(CPU_STATE_S.SET_BIT,CPU_STATE_S.CF);
+	//cpu.flags.bit.CF = 1;
+	return cpu.cnt;
+}
+
+//INC/DEC 	rm8
+static int opfe( CPU_STATE_S cpu )
+{
+	int src, cf;
+	cpu.cnt |= BCNT_1; // modrm
+	src = get_ea_val_8(cpu);
+	switch( cpu.modrm(CPU_STATE_S.GetBit, CPU_STATE_S.reg) )
+	{
+		case 0x0: // INC rm8
+			set_ea_val_8(cpu, src + 1);
+			// don't modify CF 
+			
+			cf = cpu.get_flag_bit(CPU_STATE_S.CF);
+			flags.set_flag_add_byte(cpu, src, 1);
+			cpu.flags.bit.CF = ( cf != 0 );
+			break;
+			
+		case 0x1: // DEC rm8
+			set_ea_val_8(cpu, src - 1);
+			// don't modify CF 
+			cf = cpu.get_flag_bit(CPU_STATE_S.CF);
+			flags.set_flag_sub_byte(cpu, src, 1);
+			cpu.flags.bit.CF = ( cf != 0 );		
+			break;
+			
+		default:
+			return UNHANDLED_OPCODE;
+			break;
+	}
+	return cpu.cnt;
+}
 private static void set_ea_val_8(CPU_STATE_S cpu, int val) {
-	// TODO Auto-generated method stub
+
 	int disp16 = MAKE16( cpu.mp.mp[3], cpu.mp.mp[2] );
 	int disp8 = cpu.mp.mp[2];
 	
@@ -342,7 +987,7 @@ static int get_ea_val_8( CPU_STATE_S cpu )
 }
 
 private static int op09(CPU_STATE_S cpu) {
-	// TODO Auto-generated method stub
+
 	int src1, src2, dst;
 	cpu.cnt |= BCNT_1; // modrm
 	src1 = get_ea_val_16( cpu );
@@ -350,7 +995,7 @@ private static int op09(CPU_STATE_S cpu) {
 	dst = src1 | src2;
 	set_ea_val_16(cpu, dst);
 	flags. set_flag_log_word(cpu, dst);
-	//printf( "OR %s, %s\n", wordRegName[ cpu->modrm.bit.reg ], wordRegName[ cpu->modrm.bit.rm ] );
+	//printf( "OR %s, %s\n", wordRegName[ cpu.modrm(CPU_STATE_S.GetBit, CPU_STATE_S.reg) ], wordRegName[ cpu.modrm.bit.rm ] );
 	return cpu.cnt;
 }
 
@@ -630,9 +1275,25 @@ private static int get_ea_val_16(CPU_STATE_S cpu) {
 	return 0;
 }
 
+private static int MAKE_S16(int lo) {
+	// TODO Auto-generated method stub
+	return ( ( (( lo ) & 0x80)>0 ) ? ( ( lo ) | 0xff00) : ( ( lo ) & 0xff ) );
+}
 
-private static int MAKE16(int h, int l) {
+private static int MAKE16(int h, int l)
+{
 	return (l | h << 8);
 }
+private static int MODRM_RM(int b) 
+{
+	// TODO Auto-generated method stub
+	return  (( b ) & 0x7) ;
+}
+
+
+private static int ADD_S8(int w, int b) {
+	return ( ( (( ( b ) & 0x80 )>0) ? ( ( w ) + ( ( b ) | 0xff00 ) ) : ( ( w ) + ( ( b ) & 0xff ) ) ) & 0xffff );
+}
+
 
 }
